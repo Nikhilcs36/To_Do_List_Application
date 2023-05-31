7af92d63de56d36cd5992f824ef2ad8e00b63e3e
Nikhil cs <nikhilcs36@gmail.com>
2023-05-31 21:36:39 +0530

    set up model.py and admin.py in todo_app

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import TodoItem, Tag
 
-# Register your models here.
+
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp', 'status')
+    list_filter = ('status',)
+    search_fields = ('title', 'description')
+    fieldsets = (
+        ('Task Details', {'fields': ('title', 'description')}),
+        ('Additional Info', {'fields': ('due_date', 'tags', 'status')}),
+    )
+    readonly_fields = ('timestamp',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(TodoItem)
+admin.site.register(Tag)
